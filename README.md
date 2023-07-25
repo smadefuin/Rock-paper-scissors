17b8bdd292774fdb726c4bfd2bcf2921cfb8effd
Serah Adefuin <sm.adefuin@gmail.com>
2023-07-24 21:14:06 -0600

    Linked js file to html Wrote psuedocode in js for Rock Paper Scissors breakdown

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Rock-paper-scissors
+
+First Javascript Foundations project
+
+Building a simple implementation of rock paper scissors from scratch
---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+Set a function that randomly returns 'Rock,' 'Paper,' or 'Scissors'
+Write a function that plays one round of Rock Paper Scissors
+Set two parameters for Player and Computer and return a string that declares the winner
+Make inputs case-insensitive ie. Rock, rock, Rock, or any other variations
+Write a function called game that uses a previous function to play a 5 round game that keeps score and reports a winner and loser at the end
+Use a prompt to get input from the user*/
